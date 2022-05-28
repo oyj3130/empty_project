e2057b5b338cbc41af72e7c9000275b8acd353ce
oyj31 <oyj3130@naver.com>
2022-05-28 22:57:40 +0900

    add new activity_sub

---
+++ app/src/main/java/com/ocom/empty_project/MainActivity_sub.java
@@ -0,0 +1,14 @@
+package com.ocom.empty_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_sub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_sub);
+    }
+}
