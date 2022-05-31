a19ad6f989bc6ed57477d1964ebd154ec74247fc
oyj31 <oyj3130@naver.com>
2022-05-31 23:00:00 +0900

    add moving activity sub class

--- app/src/main/java/com/ocom/empty_project/MainActivity.java
+++ app/src/main/java/com/ocom/empty_project/MainActivity.java
@@ -1,8 +1,11 @@
 package com.ocom.empty_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onbtnclick(View v) {
+        Toast.makeText(this, "저는 오오이에요.", Toast.LENGTH_LONG).show(); // Toast는 간단한 메세지를 잠깐 보여주는 역할을 수행한다.
+        Intent intent = new Intent(this, MainActivity_sub.class);
+        startActivity(intent);
     }
 }
